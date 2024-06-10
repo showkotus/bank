fe387b417128d4bfcfb267c83341d6a367c6abd1
Showkot <showkotus@gmail.com>
2024-06-10 06:39:17 +0600

    sentry

--- next.config.mjs
+++ next.config.mjs
@@ -25,7 +25,7 @@ export default withSentryConfig(
     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
     // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
+    widenClientFileUpload: false,
 
     // Transpiles SDK to be compatible with IE11 (increases bundle size)
     transpileClientSDK: true,
@@ -46,6 +46,6 @@ export default withSentryConfig(
     // See the following for more information:
     // https://docs.sentry.io/product/crons/
     // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: false,
+    automaticVercelMonitors: true,
   }
 );
